bf724c9ed85e44414826412c0bdb65c288e966ea
medelsaleh9-web <medelsaleh9@gmail.com>
2026-05-30 21:17:15 +0100

    IVAR v4 Bot - initial push

---
+++ bot/includes/handle/handleRefresh.js
@@ -0,0 +1,66 @@
+module.exports = function ({ api, models, Users, Threads, Currencies }) {
+    const logger = require("../../utils/log.js");
+    return async function ({ event }) {
+        const { threadID, logMessageType, logMessageData } = event;
+        const { setData, getData, delData, createData } = Threads;
+        try {
+            let dataThread = (await getData(threadID)).threadInfo;
+            switch (logMessageType) {
+                case "log:thread-admins": {
+                    if (logMessageData.ADMIN_EVENT == "add_admin") {
+                        dataThread.adminIDs.push({
+                            id: logMessageData.TARGET_ID
+                        })
+                    } else if (logMessageData.ADMIN_EVENT == "remove_admin") {
+                        dataThread.adminIDs = dataThread.adminIDs.filter(item => item.id != logMessageData.TARGET_ID);
+                    }
+                    logger('Refreshed admin list for thread ' + threadID, 'UPDATE DATA')
+                    await setData(threadID, { threadInfo: dataThread });
+                    break;
+                }
+                case "log:thread-name": {
+                    logger('Updated name for thread ' + threadID, 'UPDATE DATA')
+                    dataThread.threadName = event.logMessageData.name
+                    await setData(threadID, { threadInfo: dataThread });
+                    break;
+                }
+                case "log:subscribe": {
+                    if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+                        await new Promise(resolve => setTimeout(resolve, 2000));
+                        try {
+                            // Properly call handleCreateDatabase with the required context
+                            const handleCreateDB = require('./handleCreateDatabase.js')({ Users, Threads, Currencies });
+                            handleCreateDB({ event });
+                        } catch(e) {
+                            console.log('handleCreateDatabase error:', e);
+                        }
+                        return;
+                    }
+                    break;
+                }
+                
+                case 'log:unsubscribe': {
+                    if (logMessageData.leftParticipantFbId == api.getCurrentUserID()) {
+                        logger('Deleting data for thread ' + threadID, 'DELETE DATA')
+                        const index = global.data.allThreadID.findIndex(item => item == threadID);
+                        global.data.allThreadID.splice(index, 1);
+                        await delData(threadID);
+                        return
+                    } else {
+                        const index = dataThread.participantIDs.findIndex(item => item == logMessageData.leftParticipantFbId);
+                        dataThread.participantIDs.splice(index, 1);
+                        if (dataThread.adminIDs.find(i => i.id == logMessageData.leftParticipantFbId)) {
+                            dataThread.adminIDs = dataThread.adminIDs.filter(item => item.id != logMessageData.leftParticipantFbId);
+                        }
+                        logger('Removing user ' + logMessageData.leftParticipantFbId, 'DELETE DATA')
+                        await setData(threadID, { threadInfo: dataThread });
+                    }
+                    break;
+                }
+            }
+        } catch (e) {
+            console.log('Error updating data: ' + e)
+        }
+        return;
+    };
+}
